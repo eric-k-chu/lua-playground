7fa389c34d9867cd04181882e313dfa8fb7cfde4
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-02 18:55:25 -0800

    Testing variable initialization in load

--- love2d/hangman/main.lua
+++ love2d/hangman/main.lua
@@ -1,13 +1,15 @@
 function love.load()
+  world = "Hello World"
   love.graphics.setFont(love.graphics.newFont(24))
 end
 
 function love.draw()
-  love.graphics.print("Press 'a' to print Hello World!", 100, 100)
+  love.graphics.print("Press a key!", 100, 100)
 end
 
 function love.keypressed(key)
   print(key)
+  print(world)
 end
 
 function love.quit()
