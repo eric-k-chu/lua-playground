fce1b74efb56f736eda88d1445eaa6888698ea7e
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-02 18:54:05 -0800

    Testing key input and quit

--- love2d/hangman/main.lua
+++ love2d/hangman/main.lua
@@ -1,3 +1,15 @@
+function love.load()
+  love.graphics.setFont(love.graphics.newFont(24))
+end
+
 function love.draw()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.print("Press 'a' to print Hello World!", 100, 100)
+end
+
+function love.keypressed(key)
+  print(key)
+end
+
+function love.quit()
+  print("Quitting...")
 end
